1e106617a29de53aee8ae2d96497b4479f7b6e78
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-09-30 09:40:12 -0700

    Use list.files, much simpler.

--- main.R
+++ main.R
@@ -1,4 +1,2 @@
 dir <- "../../solar/data"
-years <- Sys.glob(file.path(dir, "*"))
-months <- lapply(years, FUN=function(y) Sys.glob(file.path(y, "*")))
-days <- lapply(months[[1]], FUN=function(m) Sys.glob(file.path(m, "*")))
+days <- list.files(dir, pattern='*.csv', recursive=T)
