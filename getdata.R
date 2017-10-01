ff861001ad5a13b69e2d12946b9bb986a5cec434
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-09-30 21:48:52 -0700

    Time zone clarity, and an example plot.

--- getdata.R
+++ getdata.R
@@ -1,13 +1,15 @@
+library(lubridate)
 getdata <- function() {
   dir <- "../../solar/data"
   days <- list.files(dir, pattern='*.csv', recursive=T)
 
-  data <- data.frame(date=character(0), time=character(0), kW=numeric(0), raw=integer(0))
+  data <- data.frame()
   
   for (day in days) {
     lines <- readLines(file.path(dir, day))
     lines <- lines[-(1:3)]
-    thisdata <- read.csv(text=lines)
+    thisdata <- read.csv(text=lines, stringsAsFactors=F)
+    thisdata$datetime <- with_tz(mdy_hms(paste(thisdata$date, thisdata$time)), "America/Los_Angeles")
     names(thisdata)[names(thisdata) == "power..kW."] <- "kW"
     data <- rbind(data, thisdata)
   }
--- main.R
+++ main.R
@@ -1,3 +1,8 @@
+library(ggplot2)
 source("getdata.R")
 
 data <- getdata()
+
+oneday <- data[month(data$datetime) == 9 & day(data$datetime) == 27,]
+p <- ggplot(oneday) + geom_line(aes(x=datetime, y=kW))
+print(p)
