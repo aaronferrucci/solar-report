ae3c166d75412600aa8437c4e401d05c1d3d5be4
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-10-01 20:14:06 -0700

    Plotting all data in 5-minute increments.

--- getdata.R
+++ getdata.R
@@ -7,11 +7,31 @@ getdata <- function() {
   
   for (day in days) {
     lines <- readLines(file.path(dir, day))
+    
+    # Remove firmware rev, etc.
     lines <- lines[-(1:3)]
+    
+    # Read into data.frame, convert datetime
     thisdata <- read.csv(text=lines, stringsAsFactors=F)
     thisdata$datetime <- with_tz(mdy_hms(paste(thisdata$date, thisdata$time)), "America/Los_Angeles")
+    
+    # convert to a more R-friendly name
     names(thisdata)[names(thisdata) == "power..kW."] <- "kW"
+
+    # for plotting, I think I want a "minutes in this day" field
+    thisdata$minute <- 60 * hour(thisdata$datetime) + minute(thisdata$datetime)
+    # add this day's data to the aggregate
     data <- rbind(data, thisdata)
   }
   return(data)
+}
+
+to_rect <- function(data) {
+  rect <- data.frame(datetime = data$datetime, kW = data$kW)
+  rect$hmin <- force_tz(ymd_hm(paste(date(rect$datetime), 0, 0)), "America/Los_Angeles")
+  rect$hmax <- force_tz(ymd_hm(paste(date(data$datetime), 23, 59)), "America/Los_Angeles")
+  rect$vmin <- data$minute
+  rect$vmax <- data$minute + 5
+
+  return(rect)
 }
--- main.R
+++ main.R
@@ -2,7 +2,9 @@ library(ggplot2)
 source("getdata.R")
 
 data <- getdata()
-
-oneday <- data[month(data$datetime) == 9 & day(data$datetime) == 27,]
-p <- ggplot(oneday) + geom_line(aes(x=datetime, y=kW))
+# Remove 0-kW points, to show the trend of start/end times.
+data <- data[data$kW > 0,]
+# p <- ggplot(data) + geom_point(size=4, shape=15, aes(x=date(datetime), y = minute, col=kW)) + scale_colour_gradient(low = "blue", high="red")
+rect <- to_rect(data)
+p <- ggplot(rect) + geom_rect(aes(xmin=hmin, xmax=hmax, ymin=vmin, ymax=vmax, fill=kW)) + scale_fill_gradient(low="blue", high="red")
 print(p)
