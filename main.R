c5b3c64333769e1d99537ed0d8f12e6ae3ca02a3
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-10-04 07:48:59 -0700

    Trying out text labels. geom_label doesn't support 'angle'.

--- main.R
+++ main.R
@@ -8,11 +8,24 @@ data <- data[data$kW > 0,]
 # p <- ggplot(data) + geom_point(size=4, shape=15, aes(x=date(datetime), y = minute, col=kW)) + scale_colour_gradient(low = "blue", high="red")
 rect <- to_rect(data)
 
+annotations <- data.frame(
+  x=c(
+    force_tz(ymd_hm("2017-09-01 12:01"), "America/Los_Angeles"),
+    force_tz(ymd_hm("2017-09-03 12:01"), "America/Los_Angeles")
+  ),
+  y=c(20*60),
+  label=c(
+    "smoky day",
+    "cleaned the solar panels"
+  )
+)
+
 y_ticks <- seq(7*60, 20*60, 90)
 p <- ggplot(rect) +
   ggtitle("Inverter Power (kW)") +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_rect(aes(xmin=hmin, xmax=hmax, ymin=vmin, ymax=vmax, fill=kW)) +
   scale_fill_gradient(low="blue", high="red") +
-  scale_y_continuous(breaks = y_ticks, labels = minutes_to_timestr(y_ticks))
+  scale_y_continuous(breaks = y_ticks, labels = minutes_to_timestr(y_ticks)) +
+  geom_text(data=annotations, aes(x=x, y=y, label=label), angle=45)
 print(p)
