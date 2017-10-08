9abc3959be85abd45bba33fc238cb3dce9ecebfa
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2017-10-07 23:04:11 -0700

    Pull out a 'getdata_day' routine.

--- getdata.R
+++ getdata.R
@@ -1,4 +1,23 @@
 library(lubridate)
+getdata_day <- function(dir, csv) {
+  lines <- readLines(file.path(dir, csv))
+
+  # Remove firmware rev, etc.
+  lines <- lines[-(1:3)]
+
+  # Read into data.frame, convert datetime
+  thisdata <- read.csv(text=lines, stringsAsFactors=F)
+  thisdata$datetime <- with_tz(mdy_hms(paste(thisdata$date, thisdata$time)), "America/Los_Angeles")
+
+  # convert to a more R-friendly name
+  names(thisdata)[names(thisdata) == "power..kW."] <- "kW"
+
+  # for plotting, I think I want a "minutes in this day" field
+  thisdata$minute <- 60 * hour(thisdata$datetime) + minute(thisdata$datetime)
+
+  return(thisdata)
+}
+
 getdata <- function() {
   dir <- "../../solar/data"
   days <- list.files(dir, pattern='*.csv', recursive=T)
@@ -6,20 +25,8 @@ getdata <- function() {
   data <- data.frame()
   
   for (day in days) {
-    lines <- readLines(file.path(dir, day))
-    
-    # Remove firmware rev, etc.
-    lines <- lines[-(1:3)]
-    
-    # Read into data.frame, convert datetime
-    thisdata <- read.csv(text=lines, stringsAsFactors=F)
-    thisdata$datetime <- with_tz(mdy_hms(paste(thisdata$date, thisdata$time)), "America/Los_Angeles")
-    
-    # convert to a more R-friendly name
-    names(thisdata)[names(thisdata) == "power..kW."] <- "kW"
+    thisdata <- getdata_day(dir, day)
 
-    # for plotting, I think I want a "minutes in this day" field
-    thisdata$minute <- 60 * hour(thisdata$datetime) + minute(thisdata$datetime)
     # add this day's data to the aggregate
     data <- rbind(data, thisdata)
   }
